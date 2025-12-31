de39916c5bd4b19316e60faf6a356c62ac1ecfff
Vinicius Huang <viniciushuangbr@gmail.com>
2025-12-31 11:48:44 -0300

    start course 2

---
+++ Nivel-1/Curso-2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    Filme meuFilme = new Filme();
+    meuFilme.nome = "O poderoso chefão";
+    meuFilme.anoDeLancamento = 1980;
+    meuFilme.duracaoEmMinutos = 180;
+
+    System.out.println(meuFilme.nome);
+    System.out.println(meuFilme.anoDeLancamento);
+    System.out.println(meuFilme.duracaoEmMinutos);
+  }
+}
